8c1793c0be566638c178ece5c9bc701feff789da
xfuhao7pro <xfuhao7@163.com>
2026-06-27 12:20:03 +0800

    fix: merge order migration branches

---
+++ retech-flow-api/apps/orders/migrations/0008_merge_20260627_1215.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("orders", "0005_rename_goods_image_order_snapshot_image_and_more"),
+        ("orders", "0007_orderappeal"),
+    ]
+
+    operations = []
